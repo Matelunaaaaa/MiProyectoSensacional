b99e62e2787a506ba6ae90080f2f355911a12fe8
Matelunaaaaa <benj.mateluna@duocuc.cl>
2024-06-09 18:57:17 -0400

    Todos los html se muestran

--- productos/urls.py
+++ productos/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('Principal', views.Principal, name='Principal'),
+    path('InicioSesion', views.InicioSesion, name='InicioSesion'),
+    path('ayuda', views.ayuda, name='ayuda'),
+    path('catalogo', views.catalogo, name='catalogo'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 def Principal(request):
     context={}
     return render(request, 'Principal.html', context)
+
+def InicioSesion(request):
+    context={}
+    return render(request, 'InicioSesion.html', context)
+
+def ayuda(request):
+    context={}
+    return render(request, 'ayuda.html', context)
+
+def catalogo(request):
+    context={}
+    return render(request, 'catalogo.html', context)
