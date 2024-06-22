625dfc5fb95a769935e2f91cb178fe1061ba04ba
sebastian cabrera <128442554+sebax401@users.noreply.github.com>
2024-06-22 00:01:43 -0400

    Add files via upload

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class cliente(models.Model):
+    correo = models.CharField(primary_key=True, max_length=40)
+    nombre = models.CharField(max_length=40) 
+    aparterno = models.CharField(max_length=40)
+    amaterno = models.CharField(max_length=40)
+    contraseña = models.CharField(max_length=40)
+
+    def __str__(self):
+        return str(self.nombre)+" "+str(self.aparterno)
+    
+class Genero(models.Model):
+    id_genero = models.AutoField(db_column='idGenero', primary_key=True)
+    genero = models.CharField(max_length=20, blank=False, null=False)
+    def __str__(self):
+        return str(self.genero)
