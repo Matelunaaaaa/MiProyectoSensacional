09b3dce5a7a3877ca82e090be0d2e887f8b78cbc
sebastian cabrera <128442554+sebax401@users.noreply.github.com>
2024-06-22 00:01:10 -0400

    Add files via upload
    se agrega el registro

--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import cliente, Genero
 
 # Create your views here.
 
@@ -21,3 +22,28 @@ def catalogo(request):
 def razas(request):
     context={}
     return render(request, 'razas.html', context)
+
+def registro(request):
+    if request.method is not "POST":
+        generos=Genero.objects.all()
+        context={'generos':generos}
+        return render(request, 'registrarse.html', context)
+    
+    else:
+        correo = request.POST['correo']
+        nombre = request.POST['nombre']
+        apaterno = request.POST['apaterno']
+        amaterno = request.POST['amaterno']
+        contraseña = request.POST['contraseña']
+        genero = request.POST['genero']
+
+        obGenero =Genero.objects.get(id_genero = genero)
+        obj = cliente.objects.create( correo = correo ,
+                                      nombre = nombre,
+                                      apaterno = apaterno,
+                                      amaterno = amaterno,
+                                      contraseña = contraseña,
+                                      id_genero = obGenero )
+        obj.save()
+        context={'mensaje':'registrado con exito'}
+        return render(request, 'registrarse.html', context)
